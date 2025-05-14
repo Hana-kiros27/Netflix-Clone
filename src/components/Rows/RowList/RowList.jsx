8c5c8582b28e993d436af1c0795270ba3d9aaff6
Hana Hailekiros <hanahailekiros27@gmail.com>
2025-05-14 02:27:42 +0200

    font family

--- src/components/Rows/RowList/RowList.jsx
+++ src/components/Rows/RowList/RowList.jsx
@@ -10,24 +10,46 @@ const RowList = () => {
         isLargeRow={true}
       />
       <Row
-        title="TRENDIG NOW"
+        title="Trending Now"
         fetchurl={requests.fetchTrending}
-        isLargeRow={true}
+        // isLargeRow={true}
       />
       <Row
-        title="Top rate movie"
+        title="Top Rate Movie"
         fetchurl={requests.fetchTopRatedMovies}
-        isLargeRow={true}
+        // isLargeRow={true}
       />
       <Row
-        title="Action movies"
+        title="Action Movies"
         fetchurl={requests.fetchActionMovies}
-        isLargeRow={true}
+        // isLargeRow={true}
       />
       <Row
-        title="Comedy movies"
+        title="Comedy Movies"
         fetchurl={requests.fetchComedyMovies}
-        isLargeRow={true}
+        // isLargeRow={true}
+      />
+      <Row
+        title="Horror Movies"
+        fetchurl={requests.fetchHorrorMovies}
+        // isLargeRow={true}
+      />
+      <Row
+        title="Romance Movies"
+        fetchurl={requests.fetchRomanceMovies}
+        // isLargeRow={true}
+      />
+      <Row
+        title="Documentaries"
+        fetchurl={requests.fetchDocumentaries}
+        // isLargeRow={true}
+       
+      />
+
+      <Row
+        title="Tv shows"
+        fetchurl={requests.fetchTvShow}
+        // isLargeRow={true}
       />
     </>
   );
